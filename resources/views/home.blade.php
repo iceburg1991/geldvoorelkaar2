7e7c759c13bf595189434620c57b19b0b9cce86f
iceburg1991 <ijsbrand@esser-emmerik.nl>
2015-09-06 10:10:29 +0200

    Master layout with sections added
    Signed-off-by: iceburg1991 <ijsbrand@esser-emmerik.nl>

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,54 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<!--================================================================================
-  Item Name: Materialize - Material Design Admin Template
-  Version: 2.2
-  Author: GeeksLabs
-  Author URL: http://www.themeforest.net/user/geekslabs
-================================================================================ -->
-
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="msapplication-tap-highlight" content="no">
-    <meta name="description" content="Materialize is a Material Design Admin Template,It's modern, responsive and based on Material Design by Google. ">
-    <meta name="keywords" content="materialize, admin template, dashboard template, flat admin template, responsive admin template,">
-    <title>Page Blank | Materialize - Material Design Admin Template</title>
-
-    <!-- Favicons-->
-    <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
-    <!-- Favicons-->
-    <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
-    <!-- For iPhone -->
-    <meta name="msapplication-TileColor" content="#00bcd4">
-    <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
-    <!-- For Windows Phone -->
-
-
-    <!-- CORE CSS-->
-
-    <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
-    <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection">
-
-
-    <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
-    <link href="css/prism.css" type="text/css" rel="stylesheet" media="screen,projection">
-    <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
-    <link href="js/plugins/chartist-js/chartist.min.css" type="text/css" rel="stylesheet" media="screen,projection">
-</head>
+@extends('master')
 
 
 
 
-
-
-
-
-
-
-
+@section('content')
 
 <!-- Start Page Loading -->
 <div id="loader-wrapper">
@@ -116,7 +71,7 @@
                                 <li class="divider"></li>
                                 <li><a href="#"><i class="mdi-action-lock-outline"></i> Lock</a>
                                 </li>
-                                <li><a href="#"><i class="mdi-hardware-keyboard-tab"></i> Logout</a>
+                                <li><a href="/auth/logout"><i class="mdi-hardware-keyboard-tab"></i> Logout</a>
                                 </li>
                             </ul>
                             <a class="btn-flat dropdown-button waves-effect waves-light white-text profile-btn" href="#" data-activates="profile-dropdown">{{ Auth::user()->name }}<i class="mdi-navigation-arrow-drop-down right"></i></a>
@@ -450,24 +405,9 @@
 <!-- END MAIN -->
 
 
+@endsection
 
-
-
-
-
-<!-- START FOOTER -->
-<footer class="page-footer">
-    <div class="footer-copyright">
-        <div class="container">
-            <span>Copyright © 2015 <a class="grey-text text-lighten-4" href="http://themeforest.net/user/geekslabs/portfolio?ref=geekslabs" target="_blank">GeeksLabs</a> All rights reserved.</span>
-            <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="http://geekslabs.com/">GeeksLabs</a></span>
-        </div>
-    </div>
-</footer>
-<!-- END FOOTER -->
-
-
-
+@section('js-loading')
 <!-- ================================================
 Scripts
 ================================================ -->
@@ -485,7 +425,4 @@ Scripts
 
 <!--plugins.js - Some Specific JS codes for Plugin Settings-->
 <script type="text/javascript" src="js/plugins.js"></script>
-
-</body>
-
-</html>
+@endsection
