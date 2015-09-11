f3869735de93b79158e215f5220dfe81e21cc253
iceburg1991 <ijsbrand@esser-emmerik.nl>
2015-09-11 23:20:51 +0200

    Icon added + Dynamic tab title
    Signed-off-by: iceburg1991 <ijsbrand@esser-emmerik.nl>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::get('home', ['middleware' => 'auth', function() {
-    return view('home');
+    return view('dashboard');
 }]);
 
 // Authentication routes...
