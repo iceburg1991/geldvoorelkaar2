e515245e4b4150b1d05f7f16dd9ac7d73d1e6b41
iceburg1991 <ijsbrand@esser-emmerik.nl>
2015-10-04 20:06:10 +0200

    Demo project collections
    Signed-off-by: iceburg1991 <ijsbrand@esser-emmerik.nl>

--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 
-@section('title','dashboard')
+@section('title','Projects')
 
 @section('content')
 
@@ -22,8 +22,7 @@
                         <div class="col s12 m12 l12">
                             <!--<h5 class="breadcrumbs-title">Projects</h5>-->
                             <ol class="breadcrumb">
-                                <li><a href="index.html">Dashboard</a></li>
-                                <li><a href="#">Pages</a></li>
+                                <li><a href="/home">Dashboard</a></li>
                                 <li class="active">Projects</li>
                             </ol>
                         </div>
@@ -36,9 +35,99 @@
             <!--start container-->
             <div class="container">
                 <div class="section">
-                    <p class="caption">Projects</p>
-                    <div class="divider"></div>
-                    <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+                    <div id="work-collections">
+                        <div class="row">
+                            <div class="col s12 m12 l6">
+                                <ul id="projects-collection" class="collection highlight">
+                                    <li class="collection-item avatar">
+                                        <i class="mdi-file-folder circle light-blue"></i>
+                                        <span class="collection-header">Projects</span>
+                                        <p>All Geldvoorelkaar projects</p>
+                                    </li>
+                                    <li class="collection-item" onclick="window.open('/home');">
+                                        <div class="row">
+                                            <div class="col s6">
+                                                <p class="collections-title">Project name</p>
+                                                <p class="collections-content">Start date</p>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat red">Invested</span>
+                                                <span>Amount</span>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat green">Interest</span>
+                                                <span id="project-line-1">Amount</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="collection-item">
+                                        <div class="row">
+                                            <div class="col s6">
+                                                <p class="collections-title">Project name</p>
+                                                <p class="collections-content">Start date</p>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat red">Invested</span>
+                                                <span>Amount</span>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat green">Interest</span>
+                                                <span id="project-line-1">Amount</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="collection-item">
+                                        <div class="row">
+                                            <div class="col s6">
+                                                <p class="collections-title">Project name</p>
+                                                <p class="collections-content">Start date</p>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat red">Invested</span>
+                                                <span>Amount</span>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat green">Interest</span>
+                                                <span id="project-line-1">Amount</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li class="collection-item">
+                                        <div class="row">
+                                            <div class="col s6">
+                                                <p class="collections-title">Project name</p>
+                                                <p class="collections-content">Start date</p>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat red">Invested</span>
+                                                <span>Amount</span>
+                                            </div>
+                                            <div class="col s3">
+                                                <span class="task-cat green">Interest</span>
+                                                <span id="project-line-1">Amount</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col s12 m8 l9">
+                            <div class="collection">
+                                <a href="#!" class="collection-item">Alvin</a>
+                                <a href="#!" class="collection-item active">Alvin</a>
+                                <a href="#!" class="collection-item">Alvin</a>
+                                <a href="#!" class="collection-item">Alvin</a>
+                            </div>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
             <!--end container-->
