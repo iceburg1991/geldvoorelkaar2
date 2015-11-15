5d44f641dd3104cc1f0bd342f793900f6dcb6fa9
iceburg1991 <ijsbrand@esser-emmerik.nl>
2015-11-15 01:57:12 +0100

    Project collection page made dynamically
    Signed-off-by: iceburg1991 <ijsbrand@esser-emmerik.nl>

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -46,10 +46,11 @@
                                         <a href="/projects/create" class="btn-floating btn-large waves-effect waves-light red right" style="top:28px;right:-15px;"><i class="mdi-content-add">add</i></a>
                                         <p>All Geldvoorelkaar projects</p>
                                     </li>
+                                    @foreach( $projects as $project)
                                     <li class="collection-item" onclick="window.open('/home');">
                                         <div class="row">
                                             <div class="col s6">
-                                                <p class="collections-title">Project name</p>
+                                                <p class="collections-title">{!! $project->name !!}</p>
                                                 <p class="collections-content">Start date</p>
                                             </div>
                                             <div class="col s3">
@@ -62,54 +63,7 @@
                                             </div>
                                         </div>
                                     </li>
-                                    <li class="collection-item">
-                                        <div class="row">
-                                            <div class="col s6">
-                                                <p class="collections-title">Project name</p>
-                                                <p class="collections-content">Start date</p>
-                                            </div>
-                                            <div class="col s3">
-                                                <span class="task-cat red">Invested</span>
-                                                <span>Amount</span>
-                                            </div>
-                                            <div class="col s3">
-                                                <span class="task-cat green">Interest</span>
-                                                <span id="project-line-1">Amount</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="collection-item">
-                                        <div class="row">
-                                            <div class="col s6">
-                                                <p class="collections-title">Project name</p>
-                                                <p class="collections-content">Start date</p>
-                                            </div>
-                                            <div class="col s3">
-                                                <span class="task-cat red">Invested</span>
-                                                <span>Amount</span>
-                                            </div>
-                                            <div class="col s3">
-                                                <span class="task-cat green">Interest</span>
-                                                <span id="project-line-1">Amount</span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                    <li class="collection-item">
-                                        <div class="row">
-                                            <div class="col s6">
-                                                <p class="collections-title">Project name</p>
-                                                <p class="collections-content">Start date</p>
-                                            </div>
-                                            <div class="col s3">
-                                                <span class="task-cat red">Invested</span>
-                                                <span>Amount</span>
-                                            </div>
-                                            <div class="col s3">
-                                                <span class="task-cat green">Interest</span>
-                                                <span id="project-line-1">Amount</span>
-                                            </div>
-                                        </div>
-                                    </li>
+                                    @endforeach
                                 </ul>
                             </div>
                         </div>
